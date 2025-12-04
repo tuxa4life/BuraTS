e06b1b9a128b06f220a8e35825f5d6311031deb1
Tuxa4Life <nikoloztuxa@gmail.com>
2025-12-05 02:14:29 +0400

    chores: added server url env var

--- client/src/Context/socketContext.tsx
+++ client/src/Context/socketContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect } from 'react'
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:5000')
+const socket = io(import.meta.env.VITE_RENDER_URL || 'http://localhost:5000')
 
 const SocketContext = createContext(null)
 export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
